5a8c9e0d204a9e7dd5532539c510f289ce8cc978
yo-linux <nixon2045@gmail.com>
2024-08-11 15:02:34 -0500

    se agrega la caracteristica features a la configuracion del recurso para resaltar error de ingreso a la consola de azure

--- 1_terraform_intro/main.tf
+++ 1_terraform_intro/main.tf
@@ -10,10 +10,7 @@ terraform {
 
 provider "azurerm" {
   # Configuration options
-  alias = "prueba"
-  subscription_id = var.subscription_id
-  client_id = var.client_id
-  client_secret = var.client_id  
+  features {}
 }
 
 resource "azurerm_resource_group" "pruebita-example" {
