7b818e245f6db42f3a5c7d5dc1cbf674cf420bdc
yo-linux <nixon2045@gmail.com>
2024-08-14 22:56:07 -0500

    se realiza capitulo 20 sobre las dependencias implicitas y explicitas agregando dos nuevos recursos dependiendo uno implicitamente y otro explicitamente

--- 1_terraform_intro/main.tf
+++ 1_terraform_intro/main.tf
@@ -17,3 +17,22 @@ resource "azurerm_resource_group" "pruebita-example" {
   name = "example-resource-group"
   location = "west europe"
 }
+
+
+#configuracion de dependencia implicita
+
+resource "azurerm_resource_group" "Rg2" {
+  name = "Rg2"
+  location = "west europe"
+  tags = {
+    dependency = azurerm_resource_group.pruebita-example.name
+  }
+}
+#configuracion de dependencia explicita
+resource "azurerm_resource_group" "Rg3" {
+  name = "Rg3"
+  location = "west europe"
+  depends_on = [
+    azurerm_resource_group.Rg2
+  ]
+}
