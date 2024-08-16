5434d1c6528603f3b8a495baec367c5cf5baf351
yo-linux <nixon2045@gmail.com>
2024-08-15 22:31:09 -0500

    se realiza capitulo 21 sobre variables de entrada y salida y como pueden ser usadas dentro  del codigo terraform

--- 1_terraform_intro/main.tf
+++ 1_terraform_intro/main.tf
@@ -36,3 +36,44 @@ resource "azurerm_resource_group" "Rg3" {
     azurerm_resource_group.Rg2
   ]
 }
+
+# crearemos o declararemos una variable de entrada que seria como especificar un 
+# input en python
+
+variable "image_id"{
+  type = string # siendo una variable de tipo string solo aceptara caracteres, no es una lista o aceptara numeros
+  default = "abc"
+}
+
+variable "image_id_example_0"{
+  type = list(string) #te esta forma esta declarando una variable lista de caracter string o cadenas
+  default = ["abc"] #especficiando entre corchetes que ese strinng o cadena sera el valor por defecto
+}
+
+# crearemos una variable tipo objeto donde podremos especificar propiedades especificas de ese objeto
+variable "docker_ports"{
+  type = list(object({
+    internal = number
+    external = number
+    protocol = string
+  }) )
+  default = [
+    {
+      internal = 5000
+      external = 4000
+      protocol = "tcp"
+    }
+
+  ]
+}
+
+#solicitamos con una variable el nombre que llevara el recurso a crearse en el siguiente bloque
+variable "project_name"{
+  type = string
+}
+
+
+resource "azurerm_resource_group" "variablesexample" {
+  name = var.project_name ##aqui es donde utilizamos la variable anterior para dar uso del donde que el usuario ingreso
+  location = "west europe"
+  }
