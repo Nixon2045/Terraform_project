5f7d777a873acfe3b7150f65b61f9fcc48b55c5b
Nixon.trabajo <Nixon.Betancour@co.nestle.com>
2024-08-20 13:23:49 -0500

    nada de cambios realizados

--- 1_terraform_intro/main.tf
+++ 1_terraform_intro/main.tf
@@ -70,8 +70,8 @@ variable "docker_ports"{
 #solicitamos con una variable el nombre que llevara el recurso a crearse en el siguiente bloque
 variable "project_name"{
   type = string
-  description = "Nombre de la variable que quiere crear"
-  validation {        
+  description = "Nombre de la variable que quiere crear" # ---> de aqui a terminar el bloque fue creado en la clase 22 de terraform
+  validation {                                           # 
         condition = length(var.project_name) > 4
         error_message = "El nombre es demasiado corto"
   }
@@ -81,55 +81,4 @@ variable "project_name"{
 resource "azurerm_resource_group" "variablesexample" {
   name = var.project_name ##aqui es donde utilizamos la variable anterior para dar nombre al recurso que el usuario escogio
   location = "west europe"
-}
-
-  # Clase 23 - cadenas interpoladas
-  #si quieremos reutilizar una variable para un recurso diferente 
-  #solo debemos agregar "" comillas como si fuera un string y seguido ponemos un signo de pesos $
-  #abrimos llaves {} quedando con la siguiente sintaxis "${var.project_name}_main" donde al final de la sintaxis
-  #agregamos el texto que nos ayudara a diferencia a un bloque del otro
-  # y si queremos agregar una nueva cadena interpolada la sintaxis seria "${var.project_name}_main ${var.variable_nueva}_ejemplodiferenciador"
-  #realizare el ejemplo con la variable project_name
-
-# Reutilizaremos el recurso anterior que usa la variable de project_name para tomar el nombre del recurso
-resource "azurerm_resource_group" "variablesexample2" {
-  name = "${var.project_name}_main" 
-  location = "west europe"
-}
-
-resource "azurerm_resource_group" "variablesexample3" {
-  name = "${var.project_name}_secundary"
-  location = "west europe"
-}
-
-#Clase 24 - locals values (valores locales)
-# son valores locales que se asignan o se pueden asignar a un recurso
-#realizare el ejemplo con la creacion del recurso anterior 
-
-locals{
-  RG1 = azurerm_resource_group.variablesexample.id
-  tag = "development"
-}
-
-
-resource "azurerm_resource_group" "variablesexample4" {
-  name = "${var.project_name}_secundary"
-  location = "west europe"
-  tags = {
-    "team" = local.tag
-  }
-}
-
-# Clase 25 - Propiedad Count 
-#La propiedad Count viene por defecto en cada uno de los bloques que creamos de recursos en terraform
-# y su valor por defult es 1, si cambiamos su valor a por ejemplo 3 se crearan 3 recursos con los parametros de este bloque
-# lo ejemplificare con el recurso de variableexaple5
-
-resource "azurerm_resource_group" "variablesexample5" {
-  count = 0
-  name = "${var.project_name}_secundary${count.index}"
-  location = "west europe"
-  tags = {
-    "team" = local.tag
-  }
-}
+} 
