1e15187f15f47fe7a67ada84e31941250bb15aae
yo-linux <nixon2045@gmail.com>
2024-08-31 14:56:47 -0500

    se realiza clase 32 pero sin poder ejecutar el plan

--- 2_terraform_labs/main.tf
+++ 2_terraform_labs/main.tf
@@ -19,18 +19,20 @@ resource "azurerm_resource_group" "Demo1" {
 }
 
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "prueba_vnet"
   location            = azurerm_resource_group.Demo1.location
   resource_group_name = azurerm_resource_group.Demo1.name
   address_space       = ["10.0.0.0/16"]
 
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
   tags = {
     environment = "Production"
   }
-}
+} 
+
+resource "azurerm_subnet" "subnet1" {
+    name           = "subnet1"
+    resource_group_name = azurerm_resource_group.Demo1.name
+    virtual_network_name = azurerm_virtual_network.vnet1.name
+    address_prefixes = ["10.0.1.0/24"]
+  }
