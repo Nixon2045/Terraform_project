3caff4c284d50c9af8f39281fef6070ec6e62ee6
yo-linux <nixon2045@gmail.com>
2024-08-31 22:08:49 -0500

    Clase 32 comentada y ya resuelta,  el problema se encontro en la version que utilizaba, la version ultima debia definirse el ID y la 3.115 no

--- 2_terraform_labs/main.tf
+++ 2_terraform_labs/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "4.0.1"
+      version = "3.115.0"
     }
   }
 }
@@ -12,6 +12,19 @@ provider "azurerm" {
   # Configuration options
   features{}
 }
+
+/* Clase 32 - Creando una red virtual 
+
+Para crear una vnet o red virtual en azure primero se necesita crear un grupo de recursos donde se instalara 
+la vnet, asi que primero se crea el grupo de recursos "Demo1" para que haga de esta funcion.
+Se crean luego dos bloques de recursos, una que sera la red virtual y tendra por nombre "vnet1"
+que tendra que tener unos argumentos requeridos para poder crearse como recurso como es address_space que es un rango de IP's 
+que pueden ser asignadas con este rango.
+Luego sigue el siguiente bloque donde se especifica la subnet o subred  
+que debera definirse source_group_name para especificar a que grupo de recursos va ser asignada la subnet
+y tambien virtual_network_name para definit a que red virtual pertecera la subred 
+y address_prefixes define el rango de ip que estara disponible para la subred
+*/
 
 resource "azurerm_resource_group" "Demo1" {
   name     = "demo1"
