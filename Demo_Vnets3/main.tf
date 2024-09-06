99426e40fa0912e1c3b7d88205192c0ff669c845
yo-linux <nixon2045@gmail.com>
2024-09-05 23:03:21 -0500

    se realiza clase 38 construyendo una vnet con un modulo creado de azure

--- Demo_Vnets3/main.tf
+++ Demo_Vnets3/main.tf
@@ -2,16 +2,39 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "4.0.1"
+      version = "3.11.0"
     }
   }
 }
 
 provider "azurerm" {
-  provider{}
+  features {
+
+  }
+}
+
+resource "azurerm_resource_group" "principal04" {
+  name = var.rg_name
+  location = var.rg_location
 }
 
 module "vnet" {
   source  = "Azure/vnet/azurerm"
-  version = "4.1.0"  
+  version = "4.1.0"
+
+  resource_group_name = var.rg_name
+  use_for_each        = false
+  address_space = [var.vnet_cidr_range]
+  subnet_names = var.subnet_names
+  subnet_prefixes = var.subnet_prefixes
+  vnet_name = var.vnet_name
+  vnet_location = var.rg_location
+  tags = {
+    enviroment = "dev"
+  }
+
+  depends_on = [azurerm_resource_group.principal04]
+
+
+
 }
