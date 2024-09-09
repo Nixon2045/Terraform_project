9381236e227c317b4ccfbac906aeeb081cd9ece3
Nixon.trabajo <Nixon.Betancour@co.nestle.com>
2024-09-09 17:03:42 -0500

    se realiza clase 40 sobre creacion de recurso de almacenamiento

--- Demo04/main.tf
+++ Demo04/main.tf
@@ -23,5 +23,32 @@ resource "azurerm_resource_group" "Demo04" {
 }
 
 resource "azurerm_storage_account" "sa" {
-    
-} 
+    name = ${lower(var.naming_prefix)${random_integer.rn.result}}
+    location = var.rg_location
+    acoount_tier = "standard"
+    resource_group_name = var.rg_name
+    account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "container" {
+    name = "terraform_state"
+    storage_account_name = azurerm_storage_account.sa.name
+}
+
+data "azurerm_storage_account.sas" "sas" {
+    connection_string = azurerm_storage_account.sa.primary_connection_string
+    https_only = true
+    resource_types = {
+        service = true
+        container = true
+        object = true
+    }
+    start = timestamp()
+    expire = timeadd(timestamp(), "17000h")
+    services {
+        blob = true
+        queue = false
+        table = false
+        file = false
+    }
+}
